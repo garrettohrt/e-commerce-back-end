795ddb1260524088baa03f5aaab9b229ff033adc
gmun <garrettohrt@gmail.com>
2022-11-28 11:52:09 -0600

    start category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,50 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const allCategories = await Category.findAll({
+      include: [
+        {
+          model: Product,
+        }
+      ]
+    });
+    if (!allCategories) {
+      res.status(404).json({ message: 'No categories found' });
+      return;
+    }
+    res.status(200).json(allCategories);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const oneCategory = await Category.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Product,
+        }
+      ]
+    })
+    if (!oneCategory) {
+      res.status(404).json({ message: 'No category found by that id' });
+      return;
+    }
+    res.status(200).json(oneCategory);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 router.post('/', (req, res) => {
