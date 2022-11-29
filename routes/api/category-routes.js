8bd12a8436e894256d21b4fc49e0bf4e56060ffc
gmun <garrettohrt@gmail.com>
2022-11-28 22:13:49 -0600

    create category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,16 +49,48 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  const createCategory = await Category.create({
+    category_name: req.body.category_name
+  })
+  if (!createCategory) {
+    res.status(500).json({ message: 'failed to create category' });
+    return;
+  } else res.status(201).json(createCategory);
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const updateCategory = await Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  if (!updateCategory) {
+    res.status(500).json({ message: 'failed to update category' });
+    
+  } else res.status(200).json(updateCategory);
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteCategory = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    if (!deleteCategory) {
+      res.status(500).json({ message: 'failed to delete category' });
+      
+    } else res.status(200).json(deleteCategory);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+
 });
 
 module.exports = router;
